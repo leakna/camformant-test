d9c0a2def6eeaa92c6b9a1d13c0e67f7e7cfb8bc
leakna <phornleakna@gmail.com>
2024-12-29 18:59:01 +0700

    deploy admin

--- apps/camformant-admin/src/components/Tables/Approval-Table.tsx
+++ apps/camformant-admin/src/components/Tables/Approval-Table.tsx
@@ -84,13 +84,13 @@ const ApprovalTable = () => {
   };
 
   const handlePageChange = (page: number) => {
-    console.log("in handle::::::", page);
+    console.log("in handle:::", page);
     setCurrentPage(page);
   };
 
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
-      {isLoading ? <SkeletonLoader text="loading..."/> : <></>}
+      {isLoading ? <SkeletonLoader /> : <></>}
       <div className="flex flex-col w-full">
         {/* Table Header */}
         <TableHeader />
